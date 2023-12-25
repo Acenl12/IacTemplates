3ae61e61583c7b8314800d8952e6cad453661b26
Acenl12 <38886380+Acenl12@users.noreply.github.com>
2023-12-25 08:42:53 +0100

    Update Temp.sh

--- Temp.sh
+++ Temp.sh
@@ -3,7 +3,7 @@
 for file in *.mp4; do
     if [ -e "$file" ]; then
         output_file="${file%.*}_x265.mp4"
-        ffmpeg -i "$file" -c:v libx265 -crf 28 -c:a aac -b:a 128k "$output_file"
+        ffmpeg -i "$file" -c:v libx265 -crf 21 -c:a aac -b:a 128k "$output_file"
         echo "Converted: $file to $output_file"
     fi
 done
